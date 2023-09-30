17013419d6b9416084cc2d937367df3fab82ee1b
Maksym Lenets <maximlenets@gmail.com>
2023-09-30 13:54:40 +0300

    updated error message

--- src/main/java/com/example/userdemo/constatnt/ErrorMessages.java
+++ src/main/java/com/example/userdemo/constatnt/ErrorMessages.java
@@ -7,6 +7,6 @@ public class ErrorMessages {
     public static final String WRONG_PHONE_NUMBER = "The phone number format is incorrect. "
             + "Acceptable phone number formats: +38(123)456-7890, +381234567890, 0504567890.";
     public static final String WRONG_EMAIL = "The email address can be 5 to 50 characters in "
-            + "length before the @ symbol and the domain part can be 2 to 4 characters in length.";
+            + "length and the domain part can be 2 to 4 characters in length.";
 
 }
